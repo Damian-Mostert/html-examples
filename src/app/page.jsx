38943fcf75d33bb58d77130e39593671ffd14bc4
Damian Mostert <damianmostert86@gmail.com>
2024-04-24 19:07:44 +0200

    x

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="text-red-400 text-center animate">
+    <main className="text-red-400 text-center animate h-screen items-center flex flex-wrap">
       {[0,0,0,0,0,0,0,0,0,0,0,0,0,0].map((_,x)=>{
-        return <div key={x}>
+        return <div key={x} className="w-full">
           HELLO ANGELO {x} 🤌🏻
         </div>
       })}
